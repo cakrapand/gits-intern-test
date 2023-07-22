1db702568b1ee660f95445aca87fa8c324564240
Muhammad Cakra <73237464+cakrapand@users.noreply.github.com>
2023-07-22 07:46:11 +0700

    Update DenseRanking.kt

--- DenseRanking.kt
+++ DenseRanking.kt
@@ -12,16 +12,16 @@ fun denseRanking(scores: List<Int>, gitsScores: List<Int>): String {
 
 fun main(args: Array<String>) {
 
-    val scoreSize = listOf(5, 7, 9)
+    val scoreSize = listOf(4, 7, 9)
     val scores = listOf(
-        listOf(120, 90, 90, 75, 60),
+        listOf(100, 80, 80, 70),
         listOf(100, 100, 50, 40, 40, 20, 10),
         listOf(110, 110, 100, 90, 90, 80, 75, 70, 60)
     )
 
-    val gitsSize = listOf(2, 4, 6)
+    val gitsSize = listOf(3, 4, 6)
     val gitsScores = listOf(
-        listOf(85, 125),
+        listOf(60, 70, 100),
         listOf(5, 25, 50, 120),
         listOf(50, 75, 90, 95, 105, 120)
     )
